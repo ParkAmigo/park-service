4dc988567bc2052008c66ccbd401461d0de29bdb
vijithmv499 <vijiths2c@gmail.com>
2024-07-09 15:41:24 +0530

    PLM-2 | Vijith | Address review comments

--- app/controllers/otp_requests_controller.rb
+++ app/controllers/otp_requests_controller.rb
@@ -2,14 +2,15 @@
 
 class OtpRequestsController < ApplicationController
   skip_before_action :verify_authenticity_token
+
   def create
-    otp = OtpRequest.new(mobile_number: params[:mobile_number],
-                         otp: rand(OtpRequest::OTP_LOWER_BOUND..OtpRequest::OTP_UPPER_BOUND),
-                         expire_at: OtpRequest::OTP_EXPIRY_IN_MINUTES.minutes.from_now)
-    if otp.save
-      render json: { data: { otp: otp.otp } }, status: :ok
+    otp_request = OtpRequest.new(mobile_number: params[:mobile_number],
+                                 otp: rand(OtpRequest::OTP_LOWER_BOUND..OtpRequest::OTP_UPPER_BOUND),
+                                 expire_at: OtpRequest::OTP_EXPIRY_IN_MINUTES.minutes.from_now)
+    if otp_request.save
+      render json: { data: { otp: otp_request.otp } }, status: :ok
     else
-      render json: { errors: otp.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: otp_request.errors.full_messages }, status: :unprocessable_entity
     end
   end
 end
